c5dd1bb9642e78a4e28c4121eae3f62a6ae5e2f9
Marcus Elg <melg@kth.se>
2021-10-09 12:07:03 +0200

    Rövarspråk

--- src/F1.hs
+++ src/F1.hs
@@ -14,7 +14,30 @@ fib 1 = 1
 fib 2 = 1
 fib n = fibbottomup n 2 (1, 1)
 
-rovarsprak s = s
-karpsravor s = s
+-- Röverspråk
+vowels = ['a', 'e', 'o','u','i','y']
+
+rovarsprakconsonant:: Char -> String
+rovarsprakconsonant c = [c] ++ ['o'] ++ [c]
+
+rovarsprakchar:: Char -> String
+rovarsprakchar c = if c `elem` vowels then [c] else rovarsprakconsonant c
+
+rovarsprak:: String -> String
+rovarsprak s = rovarsprakloop 0 s []
+
+rovarsprakloop:: Int -> String -> String -> String
+rovarsprakloop pos org new = if pos == (length org) then new
+    else rovarsprakloop (pos + 1) org (new ++ rovarsprakchar (org!!pos))
+
+karpsravor:: String -> String
+karpsravor s = karpsravorloop 0 s []
+
+karpsravorloop:: Int -> String -> String -> String
+karpsravorloop pos org new = if pos >= (length org) then new
+    else if (org!!pos) `elem` vowels then 
+        karpsravorloop (pos + 1) org (new ++ [org!!pos])
+        else karpsravorloop (pos + 3) org (new ++ [org!!pos])
+
 medellangd s = 1.0
 skyffla s = s
